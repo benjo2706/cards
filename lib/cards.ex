f843a2365e6e40c5495aa265f25d39a208c3d071
Benjo <benjamin@mondiako.com>
2018-10-07 16:18:22 -0400

    backup training

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,18 +1,59 @@
 defmodule Cards do
   @moduledoc """
-  Documentation for Cards.
+    Provides methods for creating and handling a deck of cards
   """
 
-  @doc """
-  Hello world.
+  def create_deck do
+    values = [
+      "Ace",
+      "Two",
+      "Three",
+      "Four",
+      "Five",
+      "Six",
+      "Seven",
+      "Eight",
+      "Nine",
+      "Ten",
+      "Jack",
+      "Queen",
+      "King"
+    ]
 
-  ## Examples
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
 
-      iex> Cards.hello()
-      :world
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
+  end
 
-  """
-  def hello do
-    :world
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    case File.read(filename) do
+      {:ok, binary} -> :erlang.binary_to_term(binary)
+      {:error, _reason} -> "That file does not exist"
+    end
+  end
+
+  def create_hand(hand_size) do
+    Cards.create_deck()
+    |> Cards.shuffle()
+    |> Cards.deal(hand_size)
   end
 end
---
+++ note.md
@@ -0,0 +1,8 @@
+Ok, le projet est pret...
+
+defmodule Cards do
+def 
+end
+
+pourquoi ca marche pas
+parce que tu es dans le fichier note.md
